f2c7e11d6c070aefbb5c8bfb157784ef5b1581f2
Lucianogalizia <lucianogalizia170@hotmail.com>
2025-12-03 07:53:11 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 PROJECT_ID = "eventos-479403"          # tu proyecto de Google Cloud
 DATASET_ID = "mie_db"                  # dataset donde guardaremos las tablas
 REGION = "southamerica-east1"          # región recomendada
-BUCKET_NAME = ""mie-fotos-ypf-luciano""      # bucket para guardar fotos
+BUCKET_NAME = "mie-fotos-ypf-luciano"     # bucket para guardar fotos
